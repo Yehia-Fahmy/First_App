37423ca3b9feaf04428f27c108164929ea012bdc
Yehia F <yehia.fahmy@uwaterloo.ca>
2021-01-13 12:51:58 -0500

    created Training.py and import statements

---
+++ Training.py
@@ -0,0 +1,5 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+import pickle
+import numpy as np
+import time as t
